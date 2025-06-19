57422090f7738aad0546721271ef285445bb7fc2
CheraHamza <cherahamza1@gmail.com>
2025-06-19 23:27:05 +0100

    Fetch and display champion cards from League of Legends API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,58 @@
 import "../styles/App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-	return <div className="App"></div>;
+	const [cards, setCards] = useState(null);
+	useEffect(() => {
+		let ignore = false;
+
+		fetch(
+			"https://ddragon.leagueoflegends.com/cdn/15.12.1/data/en_US/champion.json",
+			{ mode: "cors" }
+		)
+			.then((response) => response.json())
+			.then((json) => {
+				if (!ignore) {
+					let cards = [];
+					for (const key of Object.keys(json.data)) {
+						cards.push({
+							name: key,
+							img: `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${key}_0.jpg`,
+						});
+					}
+					setCards(cards);
+				}
+			});
+		return () => {
+			ignore = true;
+		};
+	}, []);
+
+	return (
+		<div className="App">
+			<CardList cards={cards}></CardList>
+		</div>
+	);
+}
+
+function CardList({ cards }) {
+	return (
+		<div className="card-list">
+			{cards &&
+				cards.map((card, index) => (
+					<Card key={index} img={card.img} name={card.name} />
+				))}
+		</div>
+	);
+}
+
+function Card({ img, name }) {
+	return (
+		<div className="card">
+			<img src={img} alt={name} />
+			<p>{name}</p>
+		</div>
+	);
 }
 
 export default App;
